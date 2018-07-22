d66be2389e414461222666ec68ff406c6f6caeb0
ByronJim <41176880+ByronJim@users.noreply.github.com>
2018-07-22 18:54:43 -0500

    usuario controller
    usuario controler comprobado y correcto

--- appParqueo/app/Http/Controllers/UsuarioController.php
+++ appParqueo/app/Http/Controllers/UsuarioController.php
@@ -19,6 +19,25 @@ use App\Models\Usuario;
 class UsuarioController extends Controller {
 
     //put your code here
+     public function registrarUsuario(Request $request) {
+        if ($request->isJson()) {
+            $data = $request->json()->all();
+            try {
+                $usuario = new Usuario();
+                $usuario->nombres = $data["nombre"];
+                $usuario->usuario = $data["usuario"];
+                $usuario->clave = $data["clave"];
+                $usuario->external_id = utilidades\UUID::v4();
+                $usuario->save();
+                return response()->json(["mensaje" => "Operacion exitosa", "siglas" => "OE"], 200);
+            } catch (Exception $ex) {
+                return response()->json(["mensaje" => "Faltan Datos", "siglas" => "FD"], 400);
+            }
+        } else {
+            return response()->json(["mensaje" => "La data no tiene el formato deseado", "siglas" => "DNF"], 404);
+        }
+    }
+    
     public function inicioSesionUsuario(Request $request) {
         if ($request->isJson()) {
             try {
@@ -27,7 +46,7 @@ class UsuarioController extends Controller {
                 if ($user) {
                     return response()->json(["usuario" => $user->usuario,
                                 "id" => $user->external_id,
-                                "nombre" => $user->nombre,
+                                "nombre" => $user->nombres,
                                 "token" => base64_encode($user->external_id . '--' . $user->usuario),
                                 "mensaje" => "Operacion exitosa", "siglas" => "OE"], 200);
                 } else {
@@ -41,24 +60,7 @@ class UsuarioController extends Controller {
         }
     }
 
-    public function registrarUsuario(Request $request) {
-        if ($request->isJson()) {
-            $data = $request->json()->all();
-            try {
-                $usuario = new Usuario();
-                $usuario->nombre = $data["nombre"];
-                $usuario->usuario = $data["usuario"];
-                $usuario->clave = $data["clave"];
-                $usuario->external_id = utilidades\UUID::v4();
-                $usuario->save();
-                return response()->json(["mensaje" => "Operacion exitosa", "siglas" => "OE"], 200);
-            } catch (Exception $ex) {
-                return response()->json(["mensaje" => "Faltan Datos", "siglas" => "FD"], 400);
-            }
-        } else {
-            return response()->json(["mensaje" => "La data no tiene el formato deseado", "siglas" => "DNF"], 404);
-        }
-    }
+   
 
     public function modificarUsuario (Request $request) {
 
@@ -67,7 +69,7 @@ class UsuarioController extends Controller {
             try {
                 $usuarioObjeto = Usuario::where("external_id", $data["external_id"])->first();
                 if (isset($data["nombre"])) {
-                    $usuarioObjeto->nombre = $data["nombre"];
+                    $usuarioObjeto->nombres = $data["nombre"];
                 }
                 if (isset($data["usuario"])) {
                     $usuarioObjeto->precio = $data["precio"];
