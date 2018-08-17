00171e6752cf7925853ea6da0c27b73ef330ce0b
darogel <41120975+darogel@users.noreply.github.com>
2018-08-16 22:51:24 -0500

    vistas Admin
    Vista del Login del Admin

--- resParking/app/src/main/java/com/aplicaciones/resparking/ReservacionAdd.java
+++ resParking/app/src/main/java/com/aplicaciones/resparking/ReservacionAdd.java
@@ -1,9 +1,6 @@
 package com.aplicaciones.resparking;
 
-<<<<<<< HEAD
 import android.annotation.SuppressLint;
-=======
->>>>>>> 4ba0f2640f497ae32c1166c98f3da3433d37e33c
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -13,10 +10,7 @@ import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< HEAD
 import android.widget.ImageButton;
-=======
->>>>>>> 4ba0f2640f497ae32c1166c98f3da3433d37e33c
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -31,20 +25,12 @@ public class ReservacionAdd extends AppCompatActivity implements View.OnClickLis
     private TextView txt_plazaR;
     private TextView txt_vehiculoR;
 
-<<<<<<< HEAD
     private ImageButton  btn_hEntradaR;
     private ImageButton btn_hSalidaR;
     private ImageButton btn_guardarR;
     private ImageButton btn_volverR;
 
     @SuppressLint("WrongViewCast")
-=======
-    private Button  btn_hEntrada;
-    private Button btn_hSalida;
-    private Button btn_guardarR;
-    private Button btn_volverR;
-
->>>>>>> 4ba0f2640f497ae32c1166c98f3da3433d37e33c
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +42,6 @@ public class ReservacionAdd extends AppCompatActivity implements View.OnClickLis
         etxt_hEntrada=(EditText)findViewById(R.id.etxt_hEntrada);
         etxt_hSalida=(EditText)findViewById(R.id.etxt_hSalida);
 
-<<<<<<< HEAD
         btn_hEntradaR=(ImageButton) findViewById(R.id.btn_hEntradaR);
         btn_hSalidaR=(ImageButton) findViewById(R.id.btn_hSalidaR);
         btn_guardarR=(ImageButton) findViewById(R.id.btn_guardarR);
@@ -64,15 +49,6 @@ public class ReservacionAdd extends AppCompatActivity implements View.OnClickLis
 
         btn_hEntradaR.setOnClickListener(this);
         btn_hSalidaR.setOnClickListener(this);
-=======
-        btn_hEntrada=(Button) findViewById(R.id.btn_hEntrada);
-        btn_hSalida=(Button) findViewById(R.id.btn_hSalida);
-        btn_guardarR=(Button)findViewById(R.id.btn_guardarR);
-        btn_volverR=(Button)findViewById(R.id.btn_volverR);
-
-        btn_hEntrada.setOnClickListener(this);
-        btn_hSalida.setOnClickListener(this);
->>>>>>> 4ba0f2640f497ae32c1166c98f3da3433d37e33c
 
         oyente();
 
@@ -83,11 +59,7 @@ public class ReservacionAdd extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
 
-<<<<<<< HEAD
         if (v==btn_hEntradaR){
-=======
-        if (v==btn_hEntrada){
->>>>>>> 4ba0f2640f497ae32c1166c98f3da3433d37e33c
             final Calendar c= Calendar.getInstance();
             hora=c.get(Calendar.HOUR_OF_DAY);
             minutos=c.get(Calendar.MINUTE);
@@ -106,11 +78,7 @@ public class ReservacionAdd extends AppCompatActivity implements View.OnClickLis
             },hora,minutos,false);
             timePickerDialog.show();
         }
-<<<<<<< HEAD
         if (v==btn_hSalidaR){
-=======
-        if (v==btn_hSalida){
->>>>>>> 4ba0f2640f497ae32c1166c98f3da3433d37e33c
             final Calendar c= Calendar.getInstance();
             hora=c.get(Calendar.HOUR_OF_DAY);
             minutos=c.get(Calendar.MINUTE);
